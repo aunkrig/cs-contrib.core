d5be6d950d908105877f9e823f847efba45a3e77
mecki <mecki@fbb715cc-d8d7-4726-bffe-4b10094b3a44>
2013-04-22 16:40:45 +0000

    Added alignment of method names and bodies.

--- trunk/de.unkrig.cs-contrib.core/src/de/unkrig/cscontrib/checks/Alignment.java
+++ trunk/de.unkrig.cs-contrib.core/src/de/unkrig/cscontrib/checks/Alignment.java
@@ -1,11 +1,22 @@
 
 package de.unkrig.cscontrib.checks;
 
+import static com.puppycrawl.tools.checkstyle.api.TokenTypes.ASSIGN;
+import static com.puppycrawl.tools.checkstyle.api.TokenTypes.CLASS_DEF;
+import static com.puppycrawl.tools.checkstyle.api.TokenTypes.COMMA;
+import static com.puppycrawl.tools.checkstyle.api.TokenTypes.ENUM_DEF;
+import static com.puppycrawl.tools.checkstyle.api.TokenTypes.IDENT;
+import static com.puppycrawl.tools.checkstyle.api.TokenTypes.INTERFACE_DEF;
+import static com.puppycrawl.tools.checkstyle.api.TokenTypes.METHOD_DEF;
+import static com.puppycrawl.tools.checkstyle.api.TokenTypes.PARAMETER_DEF;
+import static com.puppycrawl.tools.checkstyle.api.TokenTypes.SLIST;
+import static com.puppycrawl.tools.checkstyle.api.TokenTypes.VARIABLE_DEF;
+import static de.unkrig.cscontrib.util.AST.grandparentTypeIs;
+import static de.unkrig.cscontrib.util.AST.parentTypeIs;
+import static de.unkrig.cscontrib.util.AST.previousSiblingTypeIs;
+
 import com.puppycrawl.tools.checkstyle.api.Check;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
-import static com.puppycrawl.tools.checkstyle.api.TokenTypes.*;
-
-import static de.unkrig.cscontrib.util.AST.*;
 
 /**
  * Checks that field/parameter/variable names are aligned in one-per-line declarations.
@@ -17,6 +28,8 @@ class Alignment extends Check {
     private boolean applyToParameterName     = true;
     private boolean applyToLocalVariableName = true;
     private boolean applyToInitializer       = true;
+    private boolean applyToMethodName        = true;
+    private boolean applyToMethodBody        = true;
 
     public void
     setApplyToFieldName(boolean applyToFieldName) {
@@ -37,18 +50,31 @@ class Alignment extends Check {
     setApplyToInitializer(boolean applyToInitializer) {
         this.applyToInitializer = applyToInitializer;
     }
+    
+    public void
+    setApplyToMethodName(boolean applyToMethodName) {
+        this.applyToMethodName = applyToMethodName;
+    }
+    
+    public void
+    setApplyToMethodBody(boolean applyToMethodBody) {
+        this.applyToMethodBody = applyToMethodBody;
+    }
 
     public int[]
     getDefaultTokens() {
-        return new int[] { VARIABLE_DEF, PARAMETER_DEF };
+        return new int[] { VARIABLE_DEF, PARAMETER_DEF, METHOD_DEF };
     }
 
     DetailAST previousFieldDeclaration    = null;
-    DetailAST firstFormalParameter        = null;
+    DetailAST previousFormalParameter     = null;
     DetailAST previousVariableDeclaration = null;
+    DetailAST previousMethodDefinition    = null;
 
     public void
     visitToken(DetailAST ast) {
+
+        @SuppressWarnings("unused") ASTDumper ad = new ASTDumper(ast);
 
         switch (ast.getType()) {
 
@@ -80,14 +106,18 @@ class Alignment extends Check {
 
         case PARAMETER_DEF:
             if (this.applyToParameterName) {
-                if (ast.getPreviousSibling() == null) {
-                    this.firstFormalParameter = ast;
-                    return;
-                }
                     
                 // Non-first parameter declaration.
-                checkDeclarationAlignment(this.firstFormalParameter, ast);
+                checkDeclarationAlignment(this.previousFormalParameter, ast);
+                this.previousFormalParameter = ast;
                 return;
+            }
+            break;
+
+        case METHOD_DEF:
+            if (this.applyToMethodName) {
+                checkMethodDefinitionAlignment(this.previousMethodDefinition, ast);
+                this.previousMethodDefinition = ast;
             }
             break;
         }
@@ -96,7 +126,7 @@ class Alignment extends Check {
     @Override public void
     beginTree(DetailAST aRootAST) {
         this.previousFieldDeclaration = null;
-        this.firstFormalParameter = null;
+        this.previousFormalParameter = null;
         this.previousVariableDeclaration = null;
     }
 
@@ -106,11 +136,10 @@ class Alignment extends Check {
      * Does nothing if {@code previousDeclaration} is {@code null}.
      */
     private void
-    checkDeclarationAlignment(
-        DetailAST previousDeclaration,
-        DetailAST currentDeclaration
-    ) {
+    checkDeclarationAlignment(DetailAST previousDeclaration, DetailAST currentDeclaration) {
         if (previousDeclaration == null) return;
+
+        if (currentDeclaration.getParent() != previousDeclaration.getParent()) return;
 
         // Apply alignment check only to first declarator of a declaration, e.g. "int a = 3, b = 7".
         if (previousDeclaration.getLineNo() == currentDeclaration.getLineNo()) return;
@@ -126,6 +155,32 @@ class Alignment extends Check {
             checkTokenAlignment(
                 previousDeclaration.findFirstToken(ASSIGN),
                 currentDeclaration.findFirstToken(ASSIGN)
+            );
+        }
+    }
+    
+    /**
+     * Logs problems iff the names or the bodies of the two method declarations are not vertically aligned.
+     * <p>
+     * Does nothing if {@code firstDefinition} is {@code null}.
+     */
+    private void
+    checkMethodDefinitionAlignment(DetailAST previousDefinition, DetailAST currentDefinition) {
+        if (previousDefinition == null) return;
+
+        @SuppressWarnings("unused") ASTDumper d = new ASTDumper(currentDefinition);
+
+        // Check vertical alignment of names.
+        checkTokenAlignment(
+            previousDefinition.findFirstToken(IDENT),
+            currentDefinition.findFirstToken(IDENT)
+        );
+        
+        // Check vertical alignment of initializers.
+        if (this.applyToMethodBody) {
+            checkTokenAlignment(
+                previousDefinition.findFirstToken(SLIST),
+                currentDefinition.findFirstToken(SLIST)
             );
         }
     }
