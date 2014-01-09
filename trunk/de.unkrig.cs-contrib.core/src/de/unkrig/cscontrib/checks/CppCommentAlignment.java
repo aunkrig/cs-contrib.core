0eab8017b112372e3f2b88d6ae91f5a2a4b6c7f4
mecki <mecki@fbb715cc-d8d7-4726-bffe-4b10094b3a44>
2014-01-09 14:39:20 +0000

    Added the 'de.unkrig.CppCommentAlignment' check.

--- trunk/de.unkrig.cs-contrib.core/src/de/unkrig/cscontrib/checks/CppCommentAlignment.java
+++ trunk/de.unkrig.cs-contrib.core/src/de/unkrig/cscontrib/checks/CppCommentAlignment.java
@@ -26,6 +26,10 @@
 
 package de.unkrig.cscontrib.checks;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 import org.apache.commons.beanutils.ConversionException;
 
 import com.google.common.collect.ImmutableMap;
@@ -35,6 +39,15 @@ import com.puppycrawl.tools.checkstyle.checks.AbstractFormatCheck;
 
 import de.unkrig.commons.nullanalysis.NotNullByDefault;
 
+/**
+ * Checks whether C++ comments are correctly aligned.
+ * <p>
+ * C++ comments must appear on the same column iff
+ * <ul>
+ *   <li>They appear in immediately consecutive lines
+ *   <li>All of these lines are of the same {@link LineKind kind}
+ * </ul>
+ */
 @NotNullByDefault(false) public
 class CppCommentAlignment extends AbstractFormatCheck {
 
@@ -52,22 +65,78 @@ class CppCommentAlignment extends AbstractFormatCheck {
         String[]                         lines       = getFileContents().getLines();
         ImmutableMap<Integer, TextBlock> cppComments = getFileContents().getCppComments();
 
-        int previousLineCppCommentColNo = -1;
-        for (int lineNo = 0; lineNo < lines.length; lineNo++) {
-            TextBlock cppComment = cppComments.get(lineNo);
-            if (cppComment == null) {
-                previousLineCppCommentColNo = -1;
-            } else {
-                int cppCommentColNo = cppComment.getStartColNo();
-                if (
-                    cppCommentColNo != 0
-                    && previousLineCppCommentColNo != -1
-                    && cppCommentColNo != previousLineCppCommentColNo
-                ) {
-                    log(lineNo, cppCommentColNo, "C++ comment is misaligned");
+        LINES:
+        for (int lineNo = 0;;) {
+            int           maxCppCommentColNo;
+            LineKind      lineKind;
+            List<Integer> cppCommentColNos = new ArrayList<Integer>();
+
+            // Find next C++ comment.
+            for (;; lineNo++) {
+
+                if (lineNo >= lines.length) break LINES;
+
+                TextBlock cppComment = cppComments.get(lineNo + 1);
+                if (cppComment != null) {
+                    maxCppCommentColNo = cppComment.getStartColNo();
+                    lineKind           = getLineKind(lines[lineNo]);
+                    cppCommentColNos.add(maxCppCommentColNo);
+                    break;
                 }
-                previousLineCppCommentColNo = cppCommentColNo;
+            }
+
+            // Find C++ comments in consecutive lines of same kind.
+            for (lineNo++; lineNo < lines.length; lineNo++) {
+
+                TextBlock cppComment = cppComments.get(lineNo + 1);
+                if (cppComment == null) break;
+
+                if (getLineKind(lines[lineNo]) != lineKind) break;
+
+                int cppCommentColNo = cppComment.getStartColNo();
+                if (cppCommentColNo > maxCppCommentColNo) maxCppCommentColNo = cppCommentColNo;
+                cppCommentColNos.add(cppCommentColNo);
+            }
+
+            // Log misaligned C++ comments.
+            for (int i = 0; i < cppCommentColNos.size(); i++) {
+                int cppCommentColNo = cppCommentColNos.get(i);
+                if (cppCommentColNo != maxCppCommentColNo) {
+                    log(
+                        lineNo - cppCommentColNos.size() + i + 1,
+                        cppCommentColNo,
+                        "C++ comment must appear on column {0}, not {1}",
+                        maxCppCommentColNo + 1,
+                        cppCommentColNo + 1
+                    );
+                }
             }
         }
     }
+
+    /** @see {@link #BARE_CPP_COMMENT}, {@link #SWITCH_LABEL_AND_CPP_COMMENT}, {@link #OTHER} */
+    enum LineKind {
+
+        /** A line which contains only a C++ comment. */
+        BARE_CPP_COMMENT,
+
+        /** A line which contains only a switch label ('case x:' or 'default:') and a C++ comment. */
+        SWITCH_LABEL_AND_CPP_COMMENT,
+
+        /** Any other line. */
+        OTHER,
+    }
+
+    private LineKind
+    getLineKind(String line) {
+        return (
+            BARE_CPP_COMMENT_LINE_PATTERN.matcher(line).matches()             ? LineKind.BARE_CPP_COMMENT :
+            SWITCH_LABEL_AND_CPP_COMMENT_LINE_PATTERN.matcher(line).matches() ? LineKind.SWITCH_LABEL_AND_CPP_COMMENT :
+            LineKind.OTHER
+        );
+    }
+    private static final Pattern
+    BARE_CPP_COMMENT_LINE_PATTERN = Pattern.compile("\\s*//.*");
+    private static final Pattern
+    SWITCH_LABEL_AND_CPP_COMMENT_LINE_PATTERN = Pattern.compile("\\s*(?:case\\b[^:]*|default\\s*):\\s*//.*");
 }
