2983d6770170df00e5f5422545057250e1ac4970
Arno Unkrig <aunkrig@users.noreply.github.com>
2017-10-04 15:17:56 +0000

    Re-based to CS 8.0.0.

--- src/de/unkrig/cscontrib/filters/SuppressionLine.java
+++ src/de/unkrig/cscontrib/filters/SuppressionLine.java
@@ -362,7 +362,7 @@ class SuppressionLine extends AutomaticBean implements Filter {
 
         // Lazy update. If the first event for the current file, update file
         // contents and tag suppressions
-        final FileContents currentContents = FileContentsHolder.getContents();
+        final FileContents currentContents = FileContentsHolder.getCurrentFileContents();
         if (currentContents == null) {
             // we have no contents, so we can not filter.
             // TODO: perhaps we should notify user somehow?
